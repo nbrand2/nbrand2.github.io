13eef453dcccd26eb75058a6415ee61958ff942c
Nick B <altniklbrandis1@gmail.com>
2023-01-20 19:32:59 -0800

    added cache

--- sw.js
+++ sw.js
@@ -1,3 +1,11 @@
+// wait for install event to happen, then waitUntil() to handle install process of the app.
+// This means creating a new cache called "static" and adding a series of assets to it
+
 self.addEventListener("install", e => {
     console.log("Install!");
+    e.waitUntil(
+        caches.open("static").then(cache => { // cache name "static" could be anything
+            return cache.addAll("./", "./images/venmo.png");
+        })
+    )
 })
