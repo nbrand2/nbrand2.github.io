4ccd8d973442575ace768f856bfcfdd395fb0c36
Nick B <altniklbrandis1@gmail.com>
2023-02-02 14:41:22 -0800

    added chatgpt submit code

--- venmo.js
+++ venmo.js
@@ -13,14 +13,26 @@ else {
 
 var profile, amt, desc;
 
-function handleSubmit() {
-    profile = document.getElementById("profile").value;
-    amt = document.getElementById("amt").value;
-    desc = document.getElementById("amt").value;
-    console.log("HEY");
-    console.log(profile, amt, desc);
-    window.location.href='transaction.html';
-}
+const form = document.getElementById("form");
+const data = [];
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const username = document.getElementById("username").value;
+    const email = document.getElementById("email").value;
+    const phone = document.getElementById("phone").value;
+    data.push({ username, email, phone });
+    console.log("Data: ", data);
+  });
+
+// function handleSubmit() {
+//     profile = document.getElementById("profile").value;
+//     amt = document.getElementById("amt").value;
+//     desc = document.getElementById("amt").value;
+//     console.log("HEY");
+//     console.log(profile, amt, desc);
+//     window.location.href='transaction.html';
+// }
 
 function loadTransaction() {
     console.log("HEY");
