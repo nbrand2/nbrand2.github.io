7edc4660094ce0a97153f541614b4b4a1da4262b
Nick B <altniklbrandis1@gmail.com>
2023-02-04 15:59:20 -0800

    switched back to using cdn

--- venmo.js
+++ venmo.js
@@ -18,13 +18,13 @@ if (form) {
     console.log("Adding event listener");
     form.addEventListener("submit", (event) => {
         event.preventDefault(); // prevent default behavior (??)
-        const file_name = saveFile();
+        //const file_name = saveFile();
         let dataArray = [];
         const profile = document.getElementById("profile").value;
         const amt = document.getElementById("amt").value;
         const desc = document.getElementById("desc").value;
         const username = document.getElementById("username").value;
-        dataArray.push({ profile, amt, desc, username, file_name });
+        dataArray.push({ profile, amt, desc, username });
         console.log("Data: ", dataArray);
         window.sessionStorage.setItem("data",JSON.stringify(dataArray)); // session storage values must be strings, so stringify it
         window.location.href = 'transaction.html';
@@ -34,29 +34,29 @@ else {
     console.log("Form doesn't exist")
 }
 
-async function saveFile() {
-    const input = document.getElementById("fileupload");
+// async function saveFile() {
+//     const input = document.getElementById("fileupload");
 
-    let selectedFile = input.files[0];
+//     let selectedFile = input.files[0];
 
-    const storageRef = JSON.parse(window.sessionStorage.getItem("ref"));
-    
-    const uploadTask = storageRef.child('images/${selectedFile.name}').put(selectedFile); //create a child directory called images, and place the file inside this directory
-    uploadTask.on('state_changed', (snapshot) => {
-    // Observe state change events such as progress, pause, and resume
-    }, (error) => {
-        // Handle unsuccessful uploads
-        console.log(error);
-    }, () => {
-        // Do something once upload is complete
-        console.log('success');
-    });
-    // let formData = new FormData();
-    // formData.append("file", input.files[0]);
-    // await fetch("./upload.php", {method: "POST", body: formData});
-    // alert("The file " + input.value.substring(12) + " has been uploaded successfully");
-    // return input.value.substring(12); 
-}
+//     const storageRef = JSON.parse(window.sessionStorage.getItem("ref"));
+
+//     const uploadTask = storageRef.child('images/${selectedFile.name}').put(selectedFile); //create a child directory called images, and place the file inside this directory
+//     uploadTask.on('state_changed', (snapshot) => {
+//     // Observe state change events such as progress, pause, and resume
+//     }, (error) => {
+//         // Handle unsuccessful uploads
+//         console.log(error);
+//     }, () => {
+//         // Do something once upload is complete
+//         console.log('success');
+//     });
+//     // let formData = new FormData();
+//     // formData.append("file", input.files[0]);
+//     // await fetch("./upload.php", {method: "POST", body: formData});
+//     // alert("The file " + input.value.substring(12) + " has been uploaded successfully");
+//     // return input.value.substring(12); 
+// }
 
 
 function loadTransaction() {
@@ -68,14 +68,14 @@ function loadTransaction() {
         const desc = document.getElementById("t_desc");
         const date = document.getElementById("date");
         const paidTo = document.getElementById("paid_to");
-        const prof_pic = document.getElementById("profile_pic");
+        //const prof_pic = document.getElementById("profile_pic");
         profile.innerText = data[0]["profile"];
         amt.innerText = "- $" + data[0]["amt"];
         desc.innerText = "\"" + data[0]["desc"] + "\"";
         date.innerText = convertDate(new Date());
         paidTo.innerText = data[0]["username"];
-        prof_pic.src = "./images/" + data[0]["file_name"];
-        console.log(prof_pic.src);
+        //prof_pic.src = "./images/" + data[0]["file_name"];
+        //console.log(prof_pic.src);
         // console.log(data[0]["profile"]);
     }
     else {
